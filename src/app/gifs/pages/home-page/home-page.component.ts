1e7537f66cd67ff46ddf888a306d1ff3e89e1ced
AFOXJONES <alvarofabsas@gmail.com>
2024-06-09 20:54:54 +0200

    Creacion de interface con quicktype y mostrar lso titulos de gifs en pantalla

--- src/app/gifs/pages/home-page/home-page.component.ts
+++ src/app/gifs/pages/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
+import { Gif } from './../../interfaces/gifs.interfaces';
 import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
 
 @Component({
   selector: 'gifs-home-page',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  constructor(private gifsService:GifsService){}
+
+
+
+  get gifs():Gif[]{
+    return this.gifsService.gifList;
+  }
 }
