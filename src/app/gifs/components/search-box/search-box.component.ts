aec498e8fe626d6920d48e24593a6024f2e16970
AFOXJONES <alvarofabsas@gmail.com>
2024-06-09 19:55:31 +0200

    gifs.service y resolucion de tarea

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -17,8 +18,12 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!:ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService:GifsService){ }
+
   searchTag(){
     const newTag=this.tagInput.nativeElement.value;
-    console.log({newTag});
+    this.gifsService.serachTag(newTag);
+    this.tagInput.nativeElement.value='';
+
   }
 }
