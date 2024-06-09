2a6f276d53a6394d6b270d0b2698d315690de8fc
AFOXJONES <alvarofabsas@gmail.com>
2024-06-09 20:03:53 +0200

    logica de Search-box realizada

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,8 +13,23 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  serachTag(tag:string){
+
+  private organizeHistory(tag:string){
+    tag=tag.toLowerCase();
+
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory=this._tagsHistory.filter((oldTag)=>oldTag !==tag)
+    }
+
     this._tagsHistory.unshift(tag);
+    this._tagsHistory=this.tagsHistory.splice(0,10);
+  }
+
+  serachTag(tag:string){
+    if(tag.length===0)return;
+
+    this.organizeHistory(tag);
+
     console.log(this._tagsHistory)
   }
 
