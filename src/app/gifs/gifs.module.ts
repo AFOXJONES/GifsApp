dd8994e630caf2f6660f935af0d47d6cf213d4f1
AFOXJONES <alvarofabsas@gmail.com>
2024-06-15 11:10:57 +0200

    Preparando para en Lazyloading

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { CardComponent } from './components/card/card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -12,10 +13,11 @@ import { CardComponent } from './components/card/card.component';
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
-    CardComponent
+    CardComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports:[
     HomePageComponent
